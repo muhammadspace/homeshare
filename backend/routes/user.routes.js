74f4a1250dd35bb450f204bab409f3a983247269
Mohammad Magd <mohamed10.sw2021@fci.helwan.edu.eg>
2024-04-18 06:08:17 +0200

    backend: GET /user/:id no longer requires token authentication, implemented GET /user/(username)

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,4 +1,5 @@
-// user.routes.js
+const mongoose = require("mongoose")
+
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controller/user.controller');
@@ -9,10 +10,24 @@ router.use(express.json());
 
 router.post('/registration', UserController.register);
 router.post('/login', UserController.login);
-router.get('/user/:id', userExtractor, async (req, res) => {
-    const user = await User.findById(req.params.id)
-    console.log(user)
+router.get('/user/:identifier', async (req, res) => {
+    // determine if identifier is an id or a username
+    let user;
+    if (req.params.identifier.length === 12)
+    {
+        const identifierToObjectId = new mongoose.Types.ObjectId(req.params.identifier)
+        if (identifierToObjectId.toString() === req.params.identifier)
+            // is id
+            user = await User.findById(req.params.identifier)
+        else
+            // is username
+            user = await User.findOne({ username: req.params.identifier })
+    }
+    else
+        // is username
+        user = await User.findOne({ username: req.params.identifier })
 
+    console.log(user)
     res.status(200).json(user);
 })
 
