c9431932aa770ac169ac0bf18e0aaf5326a8214e
Mohammad Magd <mohamed10.sw2021@fci.helwan.edu.eg>
2024-05-29 16:05:10 +0300

    backend: GET /uploads/:imageId

--- backend/routes/image.routes.js
+++ backend/routes/image.routes.js
@@ -18,7 +18,6 @@ const upload = multer({ storage });
 
 router.use('/', express.static(path.join(__dirname, 'uploads')));
 
-// Endpoint to upload image
 router.post('/', upload.single('image'), async (req, res) => {
   if (req.file) {
     const image = new Image({
@@ -29,6 +28,21 @@ router.post('/', upload.single('image'), async (req, res) => {
     res.status(201).json(image);
   } else {
     res.status(400).json({ error: 'No file uploaded' });
+  }
+});
+
+router.get('/:imageId', async (req, res) => {
+  try {
+    const image = await Image.findById(req.params.imageId);
+    if (image) {
+      // Send the image file
+      res.sendFile(path.resolve(image.path));
+    } else {
+      res.status(404).json({ error: 'Image not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
   }
 });
 
